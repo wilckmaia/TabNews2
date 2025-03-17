d0169564a4dff3b424a18e13476b8a892061eab5
wilckmaia <153649327+wilckmaia@users.noreply.github.com>
2025-03-17 22:58:41 +0000

    finaliza enpoint de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,43 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  //# O código acima puxa uma informação(versão do server) do banco de dados com o database.query
+  //# E disponibiliza a exibição graças ao SHOW
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  //#o código acima especifica as informaçoes requisitadas do ...Result
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  //aqui a baixo esta a variavel (databaseOpennedConnectionsResult)
+  //onde ira usar o wait para esperar o codigo estar pronto e usar o
+  //database.query que é um comando usado para se conectar ao banco de dados
+  //O (SELECT count(*)::int  FROM pg_stat_activity WHERE datname = 'local_db';) selecionar as conexoes abertas do servidor
+  //where datname = 'local_db'  especifica onde o sql/sistema vai poder manipular
+  const databaseOpennedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int  FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  //"SELECT count(*)::int  FROM pg_stat_activity WHERE datname = 'local_db';"
+
+  const databaseOpennedConnectionsValue =
+    databaseOpennedConnectionsResult.rows[0].count;
+
   response.status(200).json({
-    chave: "Os alunos do curso.dev são acima da média",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        openned_connections: databaseOpennedConnectionsValue,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,16 @@
 test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000");
+  const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  //# Os expect mostram ao json(teste automatizado test:watch) qual é o objetivo/resultado
+  //# Que tem que ser alcançado
+  expect(responseBody.dependencies.database.version).toEqual("17.4");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.openned_connections).toEqual(1);
 });
